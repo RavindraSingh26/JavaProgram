2fe07a5674b45a1403791990a9f0c19737091a36
Ravindra Singh02 <Ravindra.singh02@godigit.com>
2021-02-08 10:47:28 +0530

    Design Pattern Commit

--- src/com/feb2021/Class_8_Feb.java
+++ src/com/feb2021/Class_8_Feb.java
@@ -1,5 +1,27 @@
 package com.feb2021;
 
+class SingletonFeb8{
+	
+	private static SingletonFeb8 instance;
+	
+	private SingletonFeb8() {
+		
+	}
+	
+	public static SingletonFeb8 getInstance() {
+		
+		if(instance == null) {
+			
+			synchronized (SingletonFeb8.class) {
+				
+				if(instance == null) {
+					instance = new SingletonFeb8();
+				}
+			}
+		}
+		return null;
+	}
+}
 public class Class_8_Feb {
 
 	public static void main(String[] args) {
--- src/com/feb2021/Second_Feb_8_to_12.java
+++ src/com/feb2021/Second_Feb_8_to_12.java
@@ -1,10 +1,17 @@
 package com.feb2021;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Second_Feb_8_to_12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// TODO : find First element from List
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		Optional<Integer> opt = list.stream().findFirst();
+		System.out.println(opt);
 	}
 
 }
